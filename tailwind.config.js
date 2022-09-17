956295ff20f88e47d5b345e7312f1bc81556d9b9
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-09-17 10:46:40 +0100

    Chore: Updated tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./sections/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				eccblue: "#0B63C5",
+				eccgreen: "#67B467",
+				clearblue: "#F1F7FE",
+				darkblue: "#031830",
+				white: "#fff",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
