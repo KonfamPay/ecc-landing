a6584005725712bbded7ff3d1779d8fdb1599428
JayB <ajiboyeabiodun001@gmail.com>
2022-09-19 16:33:07 +0200

    feat-tired

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import NavBar from "../sections/NavBar";
+import TiredOfGettingScammed from "../sections/TiredOfGettingScammed";
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<NavBar />
+			<TiredOfGettingScammed />
 		</div>
 	);
 };
