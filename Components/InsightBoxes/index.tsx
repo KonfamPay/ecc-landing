01e9095307cde9841f7cce070bf4b4e0cd7983c4
Toniodujinrin <todujinrin@gmail.com>
2022-09-20 15:00:39 +0100

    added countdown to figures

--- Components/InsightBoxes/index.tsx
+++ Components/InsightBoxes/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import CountUp from "react-countup";
 
 interface BoxProps {
 	item1: string;
-	item2: string;
+	item2: number;
 	item3: string;
-	item4: string;
+	item4: number;
 	type: string;
 }
 
@@ -17,9 +18,21 @@ const Box: React.FC<BoxProps> = ({ item1, item2, item3, item4, type }) => {
 					className="flex flex-col items-center  justify-center h-[200px] bg-white border-[3px] mr-2 border-solid border-eccblue rounded-[14px]"
 				>
 					<p className="font-semibold text-[16px]">{item1}</p>
-					<p className="font-semibold text-[20.4px] text-eccblue">{item2}</p>
+					<p className="font-semibold text-[20.4px] text-eccblue">
+						<CountUp
+							duration={10}
+							start={0}
+							end={item2}
+						></CountUp>
+					</p>
 					<p className="font-semibold text-[16px] text-center">{item3}</p>
-					<p className="font-semibold text-[20.4px] text-eccblue">{item4}</p>
+					<p className="font-semibold text-[20.4px] text-eccblue">
+						<CountUp
+							duration={10}
+							start={0}
+							end={item4}
+						></CountUp>
+					</p>
 				</div>
 			) : (
 				<div
@@ -27,9 +40,21 @@ const Box: React.FC<BoxProps> = ({ item1, item2, item3, item4, type }) => {
 					className="flex flex-col items-center  justify-center h-[160px] bg-white border-[1px] mr-2 border-solid border-eccblue rounded-[14px]"
 				>
 					<p className="font-semibold text-[12px]">{item1}</p>
-					<p className="font-semibold text-[14.4px] text-eccblue">{item2}</p>
+					<p className="font-semibold text-[14.4px] text-eccblue">
+						<CountUp
+							duration={50}
+							start={0}
+							end={item2}
+						></CountUp>
+					</p>
 					<p className="font-semibold text-[12px] w-[80%] text-center">{item3}</p>
-					<p className="font-semibold text-[14.4px] text-eccblue">{item4}</p>
+					<p className="font-semibold text-[14.4px] text-eccblue">
+						<CountUp
+							duration={50}
+							start={0}
+							end={item4}
+						></CountUp>
+					</p>
 				</div>
 			)}
 		</div>
