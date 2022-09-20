19cc2daaa5c42d1938c837d7e9860d15b94d22dc
Toniodujinrin <todujinrin@gmail.com>
2022-09-20 09:43:56 +0100

    fixed spacing

--- Components/InsightBoxes/index.tsx
+++ Components/InsightBoxes/index.tsx
@@ -12,14 +12,20 @@ const Box: React.FC<BoxProps> = ({ item1, item2, item3, item4, type }) => {
 	return (
 		<div>
 			{type == "big" ? (
-				<div style={{width:'200px'}} className="flex flex-col items-center  justify-center h-[200px] bg-white border-[3px] mr-2 border-solid border-eccblue rounded-[14px]">
+				<div
+					style={{ width: "200px", marginRight: "1.5rem" }}
+					className="flex flex-col items-center  justify-center h-[200px] bg-white border-[3px] mr-2 border-solid border-eccblue rounded-[14px]"
+				>
 					<p className="font-semibold text-[16px]">{item1}</p>
 					<p className="font-semibold text-[20.4px] text-eccblue">{item2}</p>
 					<p className="font-semibold text-[16px] text-center">{item3}</p>
 					<p className="font-semibold text-[20.4px] text-eccblue">{item4}</p>
 				</div>
 			) : (
-				<div style={{width:'175px'}} className="flex flex-col items-center  justify-center h-[160px] bg-white border-[1px] mr-2 border-solid border-eccblue rounded-[14px]">
+				<div
+					style={{ width: "175px" }}
+					className="flex flex-col items-center  justify-center h-[160px] bg-white border-[1px] mr-2 border-solid border-eccblue rounded-[14px]"
+				>
 					<p className="font-semibold text-[12px]">{item1}</p>
 					<p className="font-semibold text-[14.4px] text-eccblue">{item2}</p>
 					<p className="font-semibold text-[12px] w-[80%] text-center">{item3}</p>
